a7e644645993d2ff24c9edb8851c35e357c94f33
anedanman <anedanman@gmail.com>
2023-04-04 00:30:20 +0300

    adv nn

--- neural_astar/planner/encoder.py
+++ neural_astar/planner/encoder.py
@@ -9,6 +9,11 @@ import torch
 import torch.nn as nn
 import segmentation_models_pytorch as smp
 
+from modules.encoder import Encoder
+from modules.decoder import Decoder
+from modules.attention import SpatialTransformer
+from modules.pos_emb import PosEmbeds
+
 
 class EncoderBase(nn.Module):
     def __init__(self,
@@ -81,4 +86,55 @@ class CNN(EncoderBase):
             blocks.append(nn.Conv2d(channels[i], channels[i + 1], 3, 1, 1))
             blocks.append(nn.BatchNorm2d(channels[i + 1]))
             blocks.append(nn.ReLU())
-        return nn.Sequential(*blocks[:-1])
+        # return nn.Sequential(*blocks[:-1])
+        return Autoencoder(in_channels=4, resolution=(128, 128))
+    
+
+class Autoencoder(nn.Module):
+    def __init__(self, 
+                in_channels=2, 
+                out_channels=1, 
+                hidden_channels=64,
+                attn_blocks=4,
+                attn_heads=4,
+                cnn_dropout=0.15,
+                attn_dropout=0.15,
+                downsample_steps=3, 
+                resolution=(64, 64),
+                mode='f',
+                *args,
+                **kwargs):
+        super().__init__()
+        heads_dim = hidden_channels // attn_heads
+        self.encoder = Encoder(in_channels, hidden_channels, downsample_steps, cnn_dropout)
+        self.pos = PosEmbeds(
+            hidden_channels, 
+            (resolution[0] // 2**downsample_steps, resolution[1] // 2**downsample_steps)
+        )
+        self.transformer = SpatialTransformer(
+            hidden_channels, 
+            attn_heads,
+            heads_dim,
+            attn_blocks, 
+            attn_dropout
+        )
+        self.decoder_pos = PosEmbeds(
+            hidden_channels, 
+            (resolution[0] // 2**downsample_steps, resolution[1] // 2**downsample_steps)
+        )
+        self.decoder = Decoder(hidden_channels, out_channels, downsample_steps, cnn_dropout)
+        
+        self.recon_criterion = nn.L1Loss() if mode == 'h' else nn.MSELoss()
+        self.mode = mode
+        self.k = 64*64 if mode == 'h' else 1
+        
+        self.automatic_optimization = False
+        self.save_hyperparameters()
+
+    def forward(self, inputs):
+        x = self.encoder(inputs)
+        x = self.pos(x)
+        x = self.transformer(x)
+        x = self.decoder_pos(x)
+        x = self.decoder(x)
+        return x
